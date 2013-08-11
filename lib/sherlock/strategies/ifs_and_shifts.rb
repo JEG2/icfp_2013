67227c8ec8071a97bfe76a647542a616b9b66337
Paul Dawson <excidere@gmail.com>
2013-08-11 16:51:52 -0500

    not being stupid

--- lib/sherlock/strategies/ifs_and_shifts.rb
+++ lib/sherlock/strategies/ifs_and_shifts.rb
@@ -1,21 +1,29 @@
 module Sherlock
   module Strategies
     class IfsAndShifts < Strategy
-      SAFE_OPERATORS = %w[ not shl1 shr1 shr4 shr16 and or xor plus if0 ]
+      BINARY_OPERATORS = %w[ and or xor plus ]
+      UNARY_OPERATORS  = %w[ not shl1 shr1 shr4 shr16 ]
 
       def self.can_handle?(problem)
+        problem = Marshal.load(Marshal.dump(problem))
         problem["operators"].size == 2 &&
           problem["operators"].include?("if0") &&
-          problem["size"] == 6 && problem["operators"].include?("")
+        ((problem["size"] == 6 && unary_operators?(problem)) ||
+         (problem["size"] == 7 && binary_operators?(problem)))
+      end
 
+      def self.unary_operators?(problem)
+        problem["operators"].delete("if0")
+        UNARY_OPERATORS.include?(problem["operators"].first)
+      end
+
+      def self.binary_operators?(problem)
+        problem["operators"].delete("if0")
+        BINARY_OPERATORS.include?(problem["operators"].first)
       end
 
       def solve
-        orders = [ "(plus e e)", "e", "e" ].permutation(3).to_a.uniq.map { |order|
-          "#{order.join(" ")}"}
-        array = %w[ x 1 0 ].flat_map { |x| [x] * 4}.permutation(4).to_a.uniq
-        permutations = orders.flat_map { |order| array.map { |vars| vars.inject(order) { |program, var| program.sub("e", var)}}}
-        
+        problem["operators"].delete("if0")
         if problem["size"] == 7
           programs = solve_binary
         else
@@ -31,15 +39,28 @@ module Sherlock
         end
       end
 
-      def solve_binary(permutations)
-        possible_programs = permutations.map do |permutation|
-          "(lambda (x) (if0 )"
-        end
+      private
 
-        programs = possible_programs.map do |program|
-          Sherlock::Parser.parse(program)
-        end
+      def solve_binary
+        operator = problem["operators"].first
+        orders = [ "(#{operator} e e)", "e", "e" ].permutation(3).to_a.uniq.map { |order|
+          "#{order.join(" ")}"}
+        array = %w[ x 1 0 ].flat_map { |x| [x] * 4}.permutation(4).to_a.uniq
+        permutations = orders.flat_map { |order| array.map { |vars| vars.inject(order) { |program, var| program.sub("e", var)}}}.uniq
+        programs = permutations.map { |expression|
+          Sherlock::Parser.parse("(lambda (x) (if0 #{expression}))") }
+      end
+
+      def solve_unary
+        operator = problem["operators"].first
+        orders = [ "(#{operator} e)", "e", "e" ].permutation(3).to_a.uniq.map { |order|
+          "#{order.join(" ")}"}
+        array = %w[ x 1 0 ].flat_map { |x| [x] * 3}.permutation(3).to_a.uniq
+        permutations = orders.flat_map { |order| array.map { |vars| vars.inject(order) { |program, var| program.sub("e", var)}}}.uniq
+        programs = permutations.map { |expression|
+          Sherlock::Parser.parse("(lambda (x) (if0 #{expression}))") }
       end
     end
   end
 end
+
